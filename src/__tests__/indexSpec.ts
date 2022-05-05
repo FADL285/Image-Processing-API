02905c6d39c2536004d2c95bce5fb48f66032234
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-05-05 22:54:50 +0200

    🚀 test: Added all endpoints test cases 🚀

--- src/__tests__/indexSpec.ts
+++ src/__tests__/indexSpec.ts
@@ -9,4 +9,24 @@ describe('Test Endpoint Response', () => {
     const response = await request.get('/');
     expect(response.statusCode).toEqual(200);
   });
+
+  it('should return 404 status code when enter not exists route ex: /fadl', async () => {
+    const response = await request.get('/fadl');
+    expect(response.statusCode).toBe(404);
+  });
+
+  it('should resize the fjord image and create the new one returned status code should be 201', async () => {
+    const response = await request.get('/api/images?filename=fjord&width=800');
+    expect(response.statusCode).toBe(201);
+  });
+
+  it('should get the fjord image from cache, returned status code should be 200', async () => {
+    const response = await request.get('/api/images?filename=fjord&width=800');
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('should return 404 status code when image not found', async () => {
+    const response = await request.get('/api/images?filename=myphoto');
+    expect(response.statusCode).toBe(404);
+  });
 });
